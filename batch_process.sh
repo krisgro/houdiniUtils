f91037fd34ffd124ecad61d9e7c425e859809df3
Kris-G <103845643+krisgro@users.noreply.github.com>
2024-03-05 12:08:59 +0100

    Delete batch_process.sh

--- batch_process.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-for filename in C:/Users/Kris/Desktop/test_objs/*; do
-    hython script.py "E:/Projects/Coding/Python_for_Production/Week_04/assetToUnrealProject/main_basic_template.hipnc" "$filename"
-done
-
-
-
